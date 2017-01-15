a22e9fd1040ca117910a91c3240fafb298cd5100
Lasem Code <miftakhul.miftakhulhudha@gmail.com>
2017-01-15 22:38:48 +0700

    add social media button

---
+++ README.md
@@ -0,0 +1,5 @@
+# login-screen
+Android UI/UX Login Screen
+
+
+<img src="https://cloud.githubusercontent.com/assets/25086018/21904298/b9c66780-d935-11e6-8605-93e9a6e35862.png" width="250" height="444">
---
+++ app/src/main/java/com/lasemcode/app/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.lasemcode.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lasemcode on 13/01/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
